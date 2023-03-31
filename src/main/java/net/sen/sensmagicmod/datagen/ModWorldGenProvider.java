3a4164c763c34dfea8d423d2cd87f838763284a3
Sen <seangalexander1@gmail.com>
2023-04-01 08:46:51 +1100

    Started World Gen

---
+++ src/main/java/net/sen/sensmagicmod/datagen/ModWorldGenProvider.java
@@ -0,0 +1,24 @@
+package net.sen.sensmagicmod.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.sen.sensmagicmod.SensMagicMod;
+import net.sen.sensmagicmod.world.ModConfiguredFeatures;
+import net.sen.sensmagicmod.world.ModPlaceFeatures;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider
+{
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlaceFeatures::bootstrap);
+    public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries)
+    {
+        super(output, registries, BUILDER, Set.of(SensMagicMod.MODID));
+    }
+}
